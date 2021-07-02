bc8db5983ebab8dd5112ae13c314d229e59e9d34
Raphael Vicente Rosa <raphael@opennetworking.org>
2021-07-02 15:36:32 -0300

    Adds onos-topo collector
    Extracts list of entities and list of relations from onos-topo

---
+++ pkg/config/defs.go
@@ -0,0 +1,11 @@
+package config
+
+// Consts define the names of the available collector names.
+// It defines those names based on the collectors available
+// at the collect package.
+const (
+	ONOSE2T        = "onos-e2t"
+	ONOSXAPPKPIMON = "onos-xappkpimon"
+	ONOSXAPPPCI    = "onos-xapppci"
+	ONOSTOPO       = "onos-topo"
+)
